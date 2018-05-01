034b463013657a78ad9d0e0c893e1abad4831bbe
Jisargu <jonathnsaa@gmail.com>
2018-05-01 13:10:54 -0500

    Introduccion a JS.  Uso de funciones y cambiar el texto de una etiqueta por id al presionar un boton con estilo de bootstrap

---
+++ js/miJS2.js
@@ -0,0 +1,6 @@
+function vamosAlBreak() {
+    document.getElementById("ejemplo").innerHTML = "Texto del break plox";
+}
+function responsividadResponsable() {
+    document.getElementById("ejemplo").innerHTML = "Texto responsivo irresponsable";
+}
